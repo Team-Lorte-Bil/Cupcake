a5f94aeaf672934c8df170081cf0cbb03b4ffaa2
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-10-30 11:46:04 +0100

    Html tag remover added.

--- src/main/java/api/Utils.java
+++ src/main/java/api/Utils.java
@@ -2,11 +2,10 @@ package api;
 
 public class Utils {
     public static String encodeHtml(String src) {
-        return src.replace("&", "&amp;")
-                .replace("<", "&lt;")
-                .replace(">", "&gt;")
-                .replace("\"", "&quot;")
-                .replace("'", "&#x27;")
-                .replace("/", "&#x2F;");
+        /*
+         * Tak https://www.tutorialspoint.com/how-to-remove-the-html-tags-from-a-given-string-in-java
+         */
+        return src.replaceAll("\\<.*?\\>", "");
     }
+    
 }
