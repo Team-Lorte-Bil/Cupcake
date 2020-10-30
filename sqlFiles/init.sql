4cc2f35eb149ed0e8f435a7c7bbe19fd7be19725
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-10-30 09:29:31 +0100

    initsql file

--- sqlFiles/init.sql
+++ sqlFiles/init.sql
@@ -1,10 +1,3 @@
-CREATE DATABASE `Cupcake` CHARACTER SET utf8 COLLATE utf8_danish_ci;
-CREATE USER 'cupcake'@'localhost' IDENTIFIED BY 'lortebil';
-GRANT ALL ON Cupcake.* TO 'cupcake'@'localhost';
-FLUSH PRIVILEGES;
-
-USE Cupcake;
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -19,32 +12,24 @@ CREATE TABLE `CakeBottoms` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
-BEGIN;
-INSERT INTO `CakeBottoms` VALUES (1, 'Chokolade', 5.00);
-INSERT INTO `CakeBottoms` VALUES (2, 'Vanilje', 5.00);
-INSERT INTO `CakeBottoms` VALUES (3, 'Nødder', 5.00);
-INSERT INTO `CakeBottoms` VALUES (4, 'Pistacie', 6.00);
-INSERT INTO `CakeBottoms` VALUES (5, 'Mandel', 7.00);
-COMMIT;
-
 -- ----------------------------
 -- Table structure for CakesOnOrder
 -- ----------------------------
 DROP TABLE IF EXISTS `CakesOnOrder`;
 CREATE TABLE `CakesOnOrder` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `orderId` int DEFAULT NULL,
+  `orderId` int NOT NULL,
   `bottomId` int NOT NULL,
   `toppingId` int NOT NULL,
-  `quantity` int NOT NULL DEFAULT '1',
+  `quantity` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `CakeIdOnOrderId` (`orderId`),
   KEY `BottomIdOnCakeId` (`bottomId`),
   KEY `ToppingIdOnCakeId` (`toppingId`),
+  KEY `CakesOnOrderId` (`orderId`),
   CONSTRAINT `BottomIdOnCakeId` FOREIGN KEY (`bottomId`) REFERENCES `CakeBottoms` (`id`),
-  CONSTRAINT `CakeIdOnOrderId` FOREIGN KEY (`orderId`) REFERENCES `Orders` (`id`),
+  CONSTRAINT `CakesOnOrderId` FOREIGN KEY (`orderId`) REFERENCES `Orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `ToppingIdOnCakeId` FOREIGN KEY (`toppingId`) REFERENCES `CakeToppings` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 -- ----------------------------
 -- Table structure for CakeToppings
@@ -56,18 +41,6 @@ CREATE TABLE `CakeToppings` (
   `price` double(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
-
-BEGIN;
-INSERT INTO `CakeToppings` VALUES (1, 'Chokolade', 5.00);
-INSERT INTO `CakeToppings` VALUES (2, 'Blåbær', 5.00);
-INSERT INTO `CakeToppings` VALUES (3, 'Hindbær', 5.00);
-INSERT INTO `CakeToppings` VALUES (4, 'Chrips', 6.00);
-INSERT INTO `CakeToppings` VALUES (5, 'Jordbær', 6.00);
-INSERT INTO `CakeToppings` VALUES (6, 'Rum og rossin', 7.00);
-INSERT INTO `CakeToppings` VALUES (7, 'Appelsin', 8.00);
-INSERT INTO `CakeToppings` VALUES (8, 'Citron', 8.00);
-INSERT INTO `CakeToppings` VALUES (9, 'Blåskimmel', 9.00);
-COMMIT;
 
 -- ----------------------------
 -- Table structure for Orders
@@ -82,8 +55,8 @@ CREATE TABLE `Orders` (
   `completed` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `UserOnOrder` (`userId`),
-  CONSTRAINT `UserOnOrder` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+  CONSTRAINT `UserOnOrder` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 -- ----------------------------
 -- Table structure for properties
@@ -93,10 +66,6 @@ CREATE TABLE `properties` (
   `name` varchar(255) COLLATE utf8_danish_ci DEFAULT NULL,
   `value` varchar(255) COLLATE utf8_danish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
-
-BEGIN;
-INSERT INTO `properties` VALUES ('version', '1');
-COMMIT;
 
 -- ----------------------------
 -- Table structure for Users
@@ -110,9 +79,27 @@ CREATE TABLE `Users` (
   `salt` blob NOT NULL,
   `secret` blob NOT NULL,
   `role` enum('User','Admin') COLLATE utf8_danish_ci NOT NULL,
-  `createdAt` timestamp NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `accountBalance` double(255,0) NOT NULL,
-  PRIMARY KEY (`id`,`email`,`phoneno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+  PRIMARY KEY (`id`,`email`,`phoneno`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+
+BEGIN;
+INSERT INTO `CakeToppings` VALUES (1, 'Chokolade', 5.00);
+INSERT INTO `CakeToppings` VALUES (2, 'Blåbær', 5.00);
+INSERT INTO `CakeToppings` VALUES (3, 'Hindbær', 5.00);
+INSERT INTO `CakeToppings` VALUES (4, 'Chrips', 6.00);
+INSERT INTO `CakeToppings` VALUES (5, 'Jordbær', 6.00);
+INSERT INTO `CakeToppings` VALUES (6, 'Rum og rossin', 7.00);
+INSERT INTO `CakeToppings` VALUES (7, 'Appelsin', 8.00);
+INSERT INTO `CakeToppings` VALUES (8, 'Citron', 8.00);
+INSERT INTO `CakeToppings` VALUES (9, 'Blåskimmel', 9.00);
+INSERT INTO `CakeBottoms` VALUES (1, 'Chokolade', 5.00);
+INSERT INTO `CakeBottoms` VALUES (2, 'Vanilje', 5.00);
+INSERT INTO `CakeBottoms` VALUES (3, 'Nødder', 5.00);
+INSERT INTO `CakeBottoms` VALUES (4, 'Pistacie', 6.00);
+INSERT INTO `CakeBottoms` VALUES (5, 'Mandel', 7.00);
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
